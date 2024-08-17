bf52e366e2ea377a353cabcec8b8b955ef97a8ac
Ayush Kumar <ayushyadav3527@gmail.com>
2024-08-17 18:18:55 +0530

    Largest number

---
+++ largestNumber.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter iize : " << endl;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "value : " << endl;
+        cin >> arr[i];
+    }
+    int mx = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > mx)
+        {
+            mx = arr[i];
+        }
+    }
+        cout << "the maximum value is : " << mx << endl;
+}
