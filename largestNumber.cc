3ff8113e86fa153ed04258aebe1c7406aae64ded
Ayush Kumar <ayushyadav3527@gmail.com>
2024-08-17 18:40:42 +0530

    Modified File

--- largestNumber.cc
+++ largestNumber.cc
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
     int n;
-    cout << "enter iize : " << endl;
+    cout << "enter size : " << endl;
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
