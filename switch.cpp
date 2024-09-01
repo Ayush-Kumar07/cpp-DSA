0e432332f6bde4e6184b2e32912705db35db1cc8
Ayush Kumar <ayushyadav3527@gmail.com>
2024-09-01 19:00:23 +0530

    File added

---
+++ switch.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+
+int main () { ;
+
+char ch = '1' ;
+cout << endl ;
+switch (ch ){
+
+    case 1 : cout<<"First " << endl ;
+            break ;
+    case '1' : cout << "Character one " << endl ;
+            break ;
+
+}
+cout<< endl ;
+return 0 ; 
+}
