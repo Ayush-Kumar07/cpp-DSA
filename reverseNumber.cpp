6bfeaad09b457d5176e27f241581a7b8be419a8c
Ayush Kumar <ayushyadav3527@gmail.com>
2024-09-01 22:04:13 +0530

    ReverseNumber

--- reverseNumber.cpp
+++ reverseNumber.cpp
@@ -8,11 +8,22 @@ int main()
     cin >> n;
 
     int rev  = 0;
+    int count = 0 ;
+    int oddcount = 0 ;
     while(n > 0)
     {
         int digit = n  % 10;
         rev = rev * 10 + digit;
         n /= 10;
+        if(digit%2==0){
+            count++;
+        } else{
+            oddcount++;
+        }
+
     }
-    cout << rev << " ";
+    cout << "reverse number are: " << rev << endl;
+    cout<< "Even number : " << count << endl ;
+    cout<< " odd number : "<< oddcount << endl ;
+
 }
