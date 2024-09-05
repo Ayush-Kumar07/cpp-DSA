cf1c9f5d6d0eb947cd3865e3cd70cc224ce7b9d2
Ayush Kumar <ayushyadav3527@gmail.com>
2024-09-06 01:27:12 +0530

    file added

---
+++ functionVoid.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void add (int num1 , int num2 ){
+    int sum = num1 + num2 ;
+    cout<<sum ;
+}
+int main(){
+    // int mysum = add(2,9) ;
+    // cout<<mysum;
+    add(2,9);
+}
