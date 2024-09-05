2347954b503509d190cd0f594227fd746f6d87c7
Ayush Kumar <ayushyadav3527@gmail.com>
2024-09-06 01:25:31 +0530

    file added

---
+++ functionpw.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int add (int num1 , int num2 ){
+    int sum = num1 + num2 ;
+    return sum ;
+}
+int main(){
+    int mysum = add(2,9) ;
+    cout<<mysum;
+}
+
+
+
+
+
